2d6a19620dbc0a212098e1d92ee0b9006cda1321
David Yu <lewen.yu@gmail.com>
2015-02-13 01:20:57 -0800

    snoc, concatv

--- src/Math/vec.hs
+++ src/Math/vec.hs
@@ -4,8 +4,9 @@ module Math.Vec where
 
 import GHC.TypeLits
 
-data Vec (n::Nat) a = Vec [a]
+data Vec (n::Nat) a = Vec [a] deriving (Show)
 
+-- unsafe, but available for convenience
 vector :: forall n a. (KnownNat n, Num a) => [a] -> Vec n a
 vector xs = Vec xs
 
@@ -14,6 +15,12 @@ nil = Vec []
 
 cons :: forall a n. (KnownNat n, Num a) => a -> Vec n a -> Vec (n+1) a
 cons x (Vec xs) = Vec (x:xs)
+
+snoc :: forall a n. (KnownNat n, Num a) => Vec n a -> a -> Vec (n+1) a
+snoc (Vec xs) x = Vec (xs ++ [x])
+
+concatv :: forall a n m. (KnownNat n, KnownNat m, Num a) => Vec n a -> Vec m a -> Vec (n+m) a
+concatv (Vec xs) (Vec ys) = Vec (xs ++ ys)
 
 type Vec2f = Vec 2 Float
 type Vec3f = Vec 3 Float
