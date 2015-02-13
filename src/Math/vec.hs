3ebdc11212a9ed25b09f310367bfd55e944d7c7b
David Yu <lewen.yu@gmail.com>
2015-02-13 01:03:07 -0800

    typed operations to compute dims at compiletime
    - major props to @bmsherman
    - I would really like fromList, but type inferrence seems to break
      with this snippet:
    
    fromList :: forall n a. (KnownNat n, Num a) => [a] -> Vec n a
    fromList [] = nil
    fromList (x : xs) = cons x $ fromList xs
    
    - but I'm not totally sure why...it's possible that GHC just doesn't know
      the size of an arbitrary list at compiletime, so all constraint checks
      fail?

--- src/Math/vec.hs
+++ src/Math/vec.hs
@@ -1,9 +1,24 @@
-{-# LANGUAGE MultiParamTypeClasses, DataKinds, KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses, DataKinds, KindSignatures, TypeOperators, ExistentialQuantification #-}
 
 module Math.Vec where
 
 import GHC.TypeLits
 
-data Vec (sz::Nat) = Vec [Double]
-type Vec3 = Vec 3
-type Vec4 = Vec 4
+data Vec (n::Nat) a = Vec [a]
+
+vector :: forall n a. (KnownNat n, Num a) => [a] -> Vec n a
+vector xs = Vec xs
+
+nil :: forall a. Num a => Vec 0 a
+nil = Vec []
+
+cons :: forall a n. (KnownNat n, Num a) => a -> Vec n a -> Vec (n+1) a
+cons x (Vec xs) = Vec (x:xs)
+
+type Vec2f = Vec 2 Float
+type Vec3f = Vec 3 Float
+type Vec4f = Vec 4 Float
+
+type Vec2 = Vec 2 Double
+type Vec3 = Vec 3 Double
+type Vec4 = Vec 4 Double
