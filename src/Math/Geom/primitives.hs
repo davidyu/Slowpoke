9cfc41986e4ceed393a027c77d0118d4d8830df2
David Yu <lewen.yu@gmail.com>
2015-05-19 01:17:13 -0700

    quick and dirty distance methods

--- src/Math/Geom/primitives.hs
+++ src/Math/Geom/primitives.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
+
 module Math.Geom.Primitives where
 
 import Math.Vec
@@ -12,3 +14,17 @@ at (Ray origin direction) t = origin + t *** direction
 infixr 5 |@|
 (|@|) :: Ray -> Double -> Point
 (|@|) = at
+
+class Distance m n where
+  dist :: m -> n -> Double
+
+instance Distance Point Point where
+  dist a b = len (a - b)
+
+instance Distance Point Plane where
+  dist pt plane = len (pt - planept) where
+                    Plane planept _ = plane
+
+instance Distance Plane Point where
+  dist plane pt = len (planept - pt) where
+                    Plane planept _ = plane
