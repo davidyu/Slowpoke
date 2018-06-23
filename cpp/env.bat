0d0740b89ef9510f190de97b4ff74c0ece50ab6c
David Yu <lewen.yu@gmail.com>
2018-06-23 13:31:47 -0700

    WIP - windows build script

---
+++ cpp/env.bat
@@ -0,0 +1,8 @@
+@echo off
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+)
+
+rem set PATH=%PATH%;C:\Program Files\Very Sleepy CS
