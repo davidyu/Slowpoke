c31b40dbf9de23dee388da17c066fef04a8952fa
David Yu <lewen.yu@gmail.com>
2018-07-11 14:17:58 -0700

    stub program branching points

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,4 +1,5 @@
 #include "bmp.h"
+#include <cstdio>
 #include <cstdint>
 #include <cstdlib>
 #include <pthread.h> 
@@ -82,105 +83,120 @@ void * startWorker( void * t ) {
 }
 
 int main( int argc, char **argv ) {
-    if ( argc >= 3 ) {
-        int w = atoi( argv[1] );
-        int h = atoi( argv[2] );
+    if ( argc > 1 ) {
+        char * mode = argv[1];
+        if ( strcmp( mode, "server" ) == 0 ) {
+            printf( "STUB server mode.\n" );
+        } else if ( strcmp( mode, "client" ) == 0 ) {
+            printf( "STUB client mode.\n" );
+        } else if ( strcmp( mode, "local" ) == 0 ) {
+            if ( argc >= 4 ) {
+                int w = atoi( argv[2] );
+                int h = atoi( argv[3] );
 
-        // parameter setup
-        params.bmp = new BMP( w, h );
+                // parameter setup
+                params.bmp = new BMP( w, h );
 
-        params.w = w;
-        params.h = h;
+                params.w = w;
+                params.h = h;
 
-        params.rt.camera.focalLength = 1.0;
-        params.rt.camera.depthOfFocus = 40.0;
-        params.rt.camera.aperture = 1.0;
-        params.rt.camera.pos.y = -2; // raise the camera up slightly
-        params.rt.camera.aspectRatio = static_cast<float>( w ) / static_cast<float>( h );
+                params.rt.camera.focalLength = 1.0;
+                params.rt.camera.depthOfFocus = 40.0;
+                params.rt.camera.aperture = 1.0;
+                params.rt.camera.pos.y = -2; // raise the camera up slightly
+                params.rt.camera.aspectRatio = static_cast<float>( w ) / static_cast<float>( h );
 
-        rt::Sphere s1( Vec3( -9, -13, -44 ), 7, new Dielectric( 1.3 ) );
-        rt::Sphere s2( Vec3( -2, -13, -88 ), 7, new Metallic( Vec4( 0.7, 0.7, 0.7, 1 ), 0.5 ) );
-        rt::Sphere s3( Vec3( 10, -13, -66 ), 7, new Lambertian( Vec4( 0.7, 0.7, 0.7, 1 ) ) );
+                rt::Sphere s1( Vec3( -9, -13, -44 ), 7, new Dielectric( 1.3 ) );
+                rt::Sphere s2( Vec3( -2, -13, -88 ), 7, new Metallic( Vec4( 0.7, 0.7, 0.7, 1 ), 0.5 ) );
+                rt::Sphere s3( Vec3( 10, -13, -66 ), 7, new Lambertian( Vec4( 0.7, 0.7, 0.7, 1 ) ) );
 
-        // cornell box faces
-        // top face
-        rt::Plane  ptop( Vec3(  0,  20, 0 ), Vec3( 0, -1, 0 ), new Lambertian( Vec4( 0.9, 0.9, 0.9, 1 ) ) );
+                // cornell box faces
+                // top face
+                rt::Plane  ptop( Vec3(  0,  20, 0 ), Vec3( 0, -1, 0 ), new Lambertian( Vec4( 0.9, 0.9, 0.9, 1 ) ) );
 
-        // bottom face
-        rt::Plane  pbot( Vec3(  0, -20, 0 ), Vec3( 0, 1, 0 ), new Lambertian( Vec4( 0.8, 0.8, 0.8, 1 ) ) );
+                // bottom face
+                rt::Plane  pbot( Vec3(  0, -20, 0 ), Vec3( 0, 1, 0 ), new Lambertian( Vec4( 0.8, 0.8, 0.8, 1 ) ) );
 
-        // back face
-        rt::Plane  pbak( Vec3(  0, 0, -65 ), Vec3( 0, 0, 1 ), new Lambertian( Vec4( 0.9, 0.9, 0.9, 1 ) ) );
+                // back face
+                rt::Plane  pbak( Vec3(  0, 0, -65 ), Vec3( 0, 0, 1 ), new Lambertian( Vec4( 0.9, 0.9, 0.9, 1 ) ) );
 
-        // side faces
-        rt::Plane  pr( Vec3(  20, 0, 0 ), Vec3( -1, 0, 0 ), new Lambertian( Vec4( 0.8, 0.3, 0.3, 1 ) ) );
-        rt::Plane  pl( Vec3( -20, 0, 0 ), Vec3(  1, 0, 0 ), new Lambertian( Vec4( 0.3, 0.8, 0.3, 1 ) ) );
+                // side faces
+                rt::Plane  pr( Vec3(  20, 0, 0 ), Vec3( -1, 0, 0 ), new Lambertian( Vec4( 0.8, 0.3, 0.3, 1 ) ) );
+                rt::Plane  pl( Vec3( -20, 0, 0 ), Vec3(  1, 0, 0 ), new Lambertian( Vec4( 0.3, 0.8, 0.3, 1 ) ) );
 
-        params.rt.scene.objects.push_back( &s1 );
-        params.rt.scene.objects.push_back( &s2 );
-        params.rt.scene.objects.push_back( &s3 );
+                params.rt.scene.objects.push_back( &s1 );
+                params.rt.scene.objects.push_back( &s2 );
+                params.rt.scene.objects.push_back( &s3 );
 
-        // params.rt.scene.objects.push_back( &ptop );
-        params.rt.scene.objects.push_back( &pbot );
-        // params.rt.scene.objects.push_back( &pbak );
-        // params.rt.scene.objects.push_back( &pl );
-        // params.rt.scene.objects.push_back( &pr );
+                // params.rt.scene.objects.push_back( &ptop );
+                params.rt.scene.objects.push_back( &pbot );
+                // params.rt.scene.objects.push_back( &pbak );
+                // params.rt.scene.objects.push_back( &pl );
+                // params.rt.scene.objects.push_back( &pr );
 
-        if ( params.singleThreaded ) {
-            Vec2 uv;
-            const Vec2 pixelSize = { 1.f / params.w, 1.f / params.h };
-            for ( int y = 0; y < params.h; y++ ) {
-                for ( int x = 0; x < params.w; x++ ) {
-                    uv.x = (float) x / params.w;
-                    uv.y = (float) y / params.h;
-                    params.bmp->SetPixel( x, y, degamma( params.rt.trace( uv, pixelSize, params.numBounces ) ) );
+                if ( params.singleThreaded ) {
+                    Vec2 uv;
+                    const Vec2 pixelSize = { 1.f / params.w, 1.f / params.h };
+                    for ( int y = 0; y < params.h; y++ ) {
+                        for ( int x = 0; x < params.w; x++ ) {
+                            uv.x = (float) x / params.w;
+                            uv.y = (float) y / params.h;
+                            params.bmp->SetPixel( x, y, degamma( params.rt.trace( uv, pixelSize, params.numBounces ) ) );
+                        }
+                    }
+                } else { // multithreaded
+                    pthread_t threads[ NUM_THREADS ];
+
+                    pthread_attr_t attr;
+                    pthread_attr_init( &attr );
+                    pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_JOINABLE );
+
+                    if ( params.useJobs ) {
+                        pthread_mutex_init( &params.jobQueue_mutex, NULL );
+                        pthread_cond_init( &params.jobQueueAvailable, NULL );
+
+                        for ( int i = 0; i < NUM_THREADS; i++ ) {
+                            uint64_t t = i;
+                            int rc = pthread_create( &threads[i], &attr, startWorker, (void *) t );
+                        }
+
+                        // add jobs to the queue
+                        for ( int y = 0; y < h; y++ ) {
+                            pthread_mutex_lock( &params.jobQueue_mutex );
+                            params.jobQueue.push( y );
+                            pthread_cond_signal( &params.jobQueueAvailable );
+                            pthread_mutex_unlock( &params.jobQueue_mutex );
+                        }
+
+                        // we're done, wake up all waiting threads
+                        pthread_mutex_lock( &params.jobQueue_mutex );
+                        params.allJobsQueued = true;
+                        pthread_cond_broadcast( &params.jobQueueAvailable );
+                        pthread_mutex_unlock( &params.jobQueue_mutex );
+                    } else { // simple thread-index based work division
+                        for ( int i = 0; i < NUM_THREADS; i++ ) {
+                            uint64_t t = i;
+                            int rc = pthread_create( &threads[i], &attr, renderTile, (void *) t );
+                        }
+                    }
+
+                    void * status;
+                    for ( int i = 0; i < NUM_THREADS; i++ ) {
+                        pthread_join( threads[i], &status );
+                    }
                 }
+
+                params.bmp->Write( "hello.bmp" );
+                // params.bmp will be automatically reclaimed by the OS when we exit, so no need to delete
+            } else {
+                printf( "usage: ray local width height\n" );
             }
-        } else { // multithreaded
-            pthread_t threads[ NUM_THREADS ];
-
-            pthread_attr_t attr;
-            pthread_attr_init( &attr );
-            pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_JOINABLE );
-
-            if ( params.useJobs ) {
-                pthread_mutex_init( &params.jobQueue_mutex, NULL );
-                pthread_cond_init( &params.jobQueueAvailable, NULL );
-
-                for ( int i = 0; i < NUM_THREADS; i++ ) {
-                    uint64_t t = i;
-                    int rc = pthread_create( &threads[i], &attr, startWorker, (void *) t );
-                }
-
-                // add jobs to the queue
-                for ( int y = 0; y < h; y++ ) {
-                    pthread_mutex_lock( &params.jobQueue_mutex );
-                    params.jobQueue.push( y );
-                    pthread_cond_signal( &params.jobQueueAvailable );
-                    pthread_mutex_unlock( &params.jobQueue_mutex );
-                }
-
-                // we're done, wake up all waiting threads
-                pthread_mutex_lock( &params.jobQueue_mutex );
-                params.allJobsQueued = true;
-                pthread_cond_broadcast( &params.jobQueueAvailable );
-                pthread_mutex_unlock( &params.jobQueue_mutex );
-            } else { // simple thread-index based work division
-                for ( int i = 0; i < NUM_THREADS; i++ ) {
-                    uint64_t t = i;
-                    int rc = pthread_create( &threads[i], &attr, renderTile, (void *) t );
-                }
-            }
-
-            void * status;
-            for ( int i = 0; i < NUM_THREADS; i++ ) {
-                pthread_join( threads[i], &status );
-            }
+        } else {
+            printf( "usage: ray [server|client|local] args\n" );
         }
-
-        params.bmp->Write( "hello.bmp" );
-
-        // params.bmp will be automatically reclaimed by the OS, so no need to delete
-        return 0;
+    } else {
+        printf( "usage: ray [server|client|local] args\n" );
     }
+
+    return 0;
 }
